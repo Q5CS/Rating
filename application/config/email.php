cd0c5e7d68ad22249fe86fecc6a6b4ca459d7e00
Fly3949 <i@fly.moe>
2019-04-10 20:56:04 +0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,13 +1,8 @@
 <?php
 $config['protocol'] = 'smtp';
-/*
-$config['smtp_host'] = 'smtp.mxhichina.com';
-$config['smtp_user'] = 'no-reply@qz5z.ren';
-$config['smtp_pass'] = 'Mail1238+1s';
-*/
-$config['smtp_host'] = 'smtpdm.aliyun.com';
-$config['smtp_user'] = 'no-reply@2018.qz5z.ren';
-$config['smtp_pass'] = 'SMTP1238plus1s';
+$config['smtp_host'] = '';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
 $config['smtp_port'] = '465';
 $config['smtp_crypto'] = 'ssl';
 $config['smtp_timeout'] = 10;
